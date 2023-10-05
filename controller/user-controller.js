c795f38c1a0a401f8af80657abdb5dc9d0980fe5
EvgeniyKaravaev <Evgeniy_900@mail.ru>
2023-10-05 20:48:20 +0300

    Выполнены изменения в файле - user-controller,добавлены блоки .catch()

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -5,17 +5,21 @@ const bcrypt = require('bcryptjs');
 const tokenKey = '1a2b-3c4d-5e6f-7g8h';
 
 const getUser = async (req, res) => {
-    await News.find({}).then((news) => { res.status(200).json(news); });
+    await News.find({}).then((news) => { res.status(200).json(news); })
+    .catch((err) => {res.status(400).json({ message: err})});
 }
 
 const getUserId = async (req, res) => {
     const id = req.params.id;
-    await News.findById(id).then((news) => { res.status(200).json(news); });
+    await News.findById(id).then((news) => { res.status(200).json(news); })
+    .catch(() => {res.status(400).json({ message: 'Проверьте корректность уникального идентификатора - ID' })});
 }
 
 const getUserDelete = async (req, res) => {
     const id = req.params.id;
-    await News.findByIdAndDelete((id)).then((result) => { res.status(200).json(result); });
+    await News.findByIdAndDelete((id))
+    .then((result) => { res.status(200).json(result); })
+    .catch(() => {res.status(400).json({ message: 'Ошибка, проверьте корректность уникального идентификатора - ID!' })});
 }
 
 const getUserPost = async (req, res) => {
@@ -65,7 +69,9 @@ const getUserPut = async (req, res) => {
 
     const newUser = new News({ first_name: userFirstName, last_name: userLastName, email: userEmail, password: userPassword });
     
-    await News.findOneAndUpdate({ _id: id }, newUser, { new: true }).then((result) => { res.status(200).json(result); });
+    await News.findOneAndUpdate({ _id: id }, newUser, { new: true })
+    .then((result) => { res.status(200).json(result); })
+    .catch(() => {res.status(400).json({ message: 'Ошибка изменения объекта!' })});
 }
 
 module.exports = {getUser, getUserId, getUserDelete, getUserPost, getUserPut};
