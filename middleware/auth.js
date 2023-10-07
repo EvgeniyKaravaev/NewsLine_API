f559016a9de4ca174ee1b497e912438874dca942
EvgeniyKaravaev <Evgeniy_900@mail.ru>
2023-10-07 20:04:04 +0300

    Добавлен middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,13 +6,13 @@ const verifyToken = (req, res, next) => {
     const token = req.body.token || req.query.token || req.headers["x-access-token"];
 
     if (!token) {
-        return res.status(403).send("A token is required for authentication");
+        return res.status(403).send("Для аутентификации необходим токен");
     }
     try {
         const decoded = jwt.verify(token, tokenKey);
         req.user = decoded;
     } catch (err) {
-        return res.status(401).send("Invalid Token");
+        return res.status(401).send("Невалидный Token");
     }
     return next();
 };
